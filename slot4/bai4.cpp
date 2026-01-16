ea303b29352d759cb845681fe4d422aa89c585e7
duong1005qb <callmegoldz22@gmail.com>
2026-01-16 11:07:35 +0700

    slot4/ngaythangnamsinh.c,bai3.c,bai1.c

---
+++ slot4/bai4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+ 	printf("nhap a la:");
+	scanf("%d",&a);
+	
+	printf("a = %d",a);
+    return 0;
+}
---
+++ slot4/ngaythangnamsinh.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int ngay,thang,nam;
+	scanf("%d/%d/%d",&ngay,&thang,&nam);
+	printf("%02d/%02d/%02d",ngay,thang,nam);
+	return 0 ;
+	
+
+}
